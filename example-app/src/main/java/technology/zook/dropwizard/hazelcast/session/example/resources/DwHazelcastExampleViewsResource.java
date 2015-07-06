41d0806c7fb1e4df8ec538b0209a10d3cbff7cf4
Martin Dow <martin.dow@gmail.com>
2015-07-06 08:07:20 +0100

    Changed example app to include a form which posts to the server to change the name in the session.

--- example-app/src/main/java/technology/zook/dropwizard/hazelcast/session/example/resources/DwHazelcastExampleViewsResource.java
+++ example-app/src/main/java/technology/zook/dropwizard/hazelcast/session/example/resources/DwHazelcastExampleViewsResource.java
@@ -3,26 +3,29 @@ package technology.zook.dropwizard.hazelcast.session.example.resources;
 import technology.zook.dropwizard.hazelcast.session.Session;
 import technology.zook.dropwizard.hazelcast.session.example.views.IndexPageView;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 
 import static javax.ws.rs.core.MediaType.TEXT_HTML;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Path("/")
 @Produces(TEXT_HTML)
 public class DwHazelcastExampleViewsResource {
 
+    public static final String NAME_PARAM = "name";
+
     @GET
-    public IndexPageView indexPage(@Context Session session, @QueryParam("name") String newName) {
-        if (isNotBlank(newName)) {
-            session.put("name", newName);
-        }
-        return new IndexPageView(session.containsKey("name") ? (String) session.get("name") : "Somebody");
+    public IndexPageView indexPage(@Context Session session) {
+        return new IndexPageView(session.containsKey(NAME_PARAM) ? (String) session.get(NAME_PARAM) : "Somebody");
+    }
+
+    @POST
+    @Path("name")
+    public Response submitName(@Context Session session, @FormParam("name") String name) {
+        session.put(NAME_PARAM, name);
+        return Response.seeOther(URI.create("/")).build();
     }
 
 }
--- example-app/src/main/resources/views/index.ftl
+++ example-app/src/main/resources/views/index.ftl
@@ -1,6 +1,12 @@
 <#-- @ftlvariable name="" type="technology.zook.dropwizard.hazelcast.example.views.IndexPageView" -->
 <html>
-    <body>
-        <h1>Hello, ${name}!</h1>
-    </body>
+  <body>
+    <h1>Hello, ${name}!</h1>
+    <h3>What to change your name?</h3>
+    <form action="/name" method="POST">
+      <label for="name">Enter name:</label>
+      <input type="text" name="name"/>
+      <button type="submit">Submit</button>
+    </form>
+  </body>
 </html>
